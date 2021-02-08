20957d30c134e324d4b0930ae48295f0aaf34ad1
stella6767 <alsrb9434@gmail.com>
2021-02-08 15:09:22 +0900

    책 CRUD 구현

--- book-frontend/src/pages/book/Detail.js
+++ book-frontend/src/pages/book/Detail.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
 
 const Detail = (props) => {
   const id = props.match.params.id;
@@ -16,9 +17,33 @@ const Detail = (props) => {
       });
   }, []);
 
+  const deleteBook = () => {
+    fetch('http://localhost:8080/book/' + id, {
+      method: 'DELETE',
+    })
+      .then((res) => res.text())
+      .then((res) => {
+        if (res === 'ok') {
+          props.history.push('/');
+        } else {
+          alert('삭제실패');
+        }
+      });
+  };
+
+  const updateBook = () => {
+    props.history.push('/updateForm/' + id);
+  };
+
   return (
     <div>
       <h1>책 상세보기</h1>
+      <Button variant="warning" onClick={updateBook}>
+        수정
+      </Button>{' '}
+      <Button variant="danger" onClick={deleteBook}>
+        삭제
+      </Button>
       <hr />
       <h3>{book.author}</h3>
       <h1>{book.title}</h1>
